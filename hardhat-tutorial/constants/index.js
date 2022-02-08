989bf68d05f92d7da6999bb5fe0954a950e337e8
Foster0x <sfoster2010.sf@gmail.com>
2022-02-07 23:33:05 -0500

    opensea

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = "0xF34D141697b728615C1cEdA8B59716Aa73d54091";
 
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-foster0x.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
